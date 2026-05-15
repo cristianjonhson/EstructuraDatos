96f9489f713cb682bd53dbfe126b2a3b4e21a768
Cristian Jonhson Alvarez <cristian.jonhson@inacapmail.cl>
2026-05-15 12:55:51 -0400

    fix: Mejorar el formato y la estructura del script de lista en SQL

--- sql/lista.sql
+++ sql/lista.sql
@@ -1,14 +1,17 @@
 -- Script de ejemplo para una estructura de datos tipo Lista en SQL
-
+DROP TABLE IF EXISTS Lista;
 CREATE TABLE Lista (
     id SERIAL PRIMARY KEY,
     valor VARCHAR(100) NOT NULL
 );
-
 -- Insertar elementos de ejemplo
-INSERT INTO Lista (valor) VALUES ('Elemento 1');
-INSERT INTO Lista (valor) VALUES ('Elemento 2');
-INSERT INTO Lista (valor) VALUES ('Elemento 3');
-
+INSERT INTO Lista (valor)
+VALUES ('Elemento 1');
+INSERT INTO Lista (valor)
+VALUES ('Elemento 2');
+INSERT INTO Lista (valor)
+VALUES ('Elemento 3');
 -- Consultar todos los elementos
-SELECT * FROM Lista;
+SELECT *
+FROM Lista
+ORDER BY id;
